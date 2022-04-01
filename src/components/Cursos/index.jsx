f15dd9277dc7c9380743c4b69c7e24e1476a5274
Gustavo Fior <gustavo_fior@outlook.com>
2022-04-01 18:32:21 -0300

    Removendo imports desnecessarios

--- src/components/Cursos/index.jsx
+++ src/components/Cursos/index.jsx
@@ -1,12 +1,9 @@
 import { Alura, Softgraf } from "../../UI/svgs";
 import React from "react";
-import { useTheme } from "styled-components";
 import CursoAlura from "../CursoAlura/index";
 import CursoSoftgraf from "../CursoSoftgraf";
 
 const Cursos = () => {
-  const theme = useTheme();
-
   return (
     <>
       <CursoAlura icone={Alura}/>
